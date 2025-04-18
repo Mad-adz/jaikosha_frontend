a358d291b48e0b5ff9e8c397fe9794770be17ad6
Mad-adz <madadzindia@gmail.com>
2025-04-18 17:32:03 +0530

    installed vite-plugin-static-copy and updated vite config js file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, normalizePath } from "vite";
 import react from "@vitejs/plugin-react";
+import { createRequire } from "module";
+
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+const require = createRequire(import.meta.url);
+
+const pdfjsDistPath = path.dirname(require.resolve("pdfjs-dist/package.json"));
+const cMapsDir = normalizePath(path.join(pdfjsDistPath, "cmaps"));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: cMapsDir,
+          dest: "",
+        },
+      ],
+    }),
+  ],
   // server: {
   //   port: 3000,
   // },
